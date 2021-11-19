c9049fd2e655ca346b90c70bab69f5a7bf9a8a7d
Meeftah0318 <47889367+Meeftah0318@users.noreply.github.com>
2021-11-19 15:40:13 +0200

    review added

--- src/Components/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Components/Pages/Dashboard/Dashboard/Dashboard.js
@@ -3,7 +3,19 @@ import React from "react";
 const Dashboard = () => {
   return (
     <div>
-      <h1>this is it</h1>
+      <div className="w-75 mx-auto border border-dark">
+        <h2 className="text-center mt-4">Dashboard</h2>
+        <div className="w-25 mx-auto mb-4">
+          <hr />
+        </div>
+        <div>
+          <img
+            className="img-fluid w-100"
+            src="https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1106&q=80"
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 };
